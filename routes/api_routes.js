42cac5280deadd2df7217c4bcbb39f52bf97b713
Donna Kuang <dolaku@gmail.com>
2019-06-28 16:02:11 -0400

    data scraped to /scrape route

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,15 +1,22 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const mongoose = require('mongoose');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
+    app.get('/scrape', (req, res) => {
+
+        // get the html from bbc page
         axios.get('https://www.bbc.com/news')
             .then(res => {
+
+                // turn that HTML into a jquery like DOM
+                // res.data === HTML
                 const $ = cheerio.load(res.data);
                 
-                // An empty array to save the data that we'll scrape
+                // an empty array to save the data that we'll scrape
                 let results = [];
 
+                // search the DOM using jquery to find all news articles
                 $('.gs-c-promo-heading__title').each((i, element) => {
                     let title = $(element).text();
                     let link = $(element).parent().attr('href');
