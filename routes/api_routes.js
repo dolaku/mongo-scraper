45372d531b6c0b47a60bae8d1a747f3c8afcd45d
Donna Kuang <dolaku@gmail.com>
2019-06-28 13:56:52 -0400

    Initial scraping of bbc news

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,5 +1,25 @@
-module.exports = function(app) {
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = (app) => {
     app.get('/', (req, res) => {
-        res.send('test');
-    });
+        axios.get('https://www.bbc.com/news')
+            .then(res => {
+                const $ = cheerio.load(res.data);
+                
+                // An empty array to save the data that we'll scrape
+                let results = [];
+
+                $('.gs-c-promo-heading__title').each((i, element) => {
+                    let title = $(element).text();
+                    let link = $(element).parent().attr('href');
+
+                    results.push({
+                        title,
+                        link
+                    });
+                });
+                console.log(results);
+            });
+        });
 }
