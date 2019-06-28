195f932c5d711318ff28457d4afe28663fed8230
Donna Kuang <dolaku@gmail.com>
2019-06-28 15:37:22 -0400

    connected html routes

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const api_routes = require('./routes/api_routes');
+const html_routes = require('./routes/html_routes');
 
 const app = express();
 
-api_routes(app);
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
 
-app.listen(PORT, () => console.log('Listening on port %s', PORT));
+api_routes(app);
+html_routes(app);
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// listen on port 8080
+app.listen(PORT, () => console.log('Listening on http://localhost:' + PORT));
