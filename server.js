b4152a150ee1103297bfe94e33c6850fca5f301a
Donna Kuang <dolaku@gmail.com>
2019-07-01 11:45:04 -0400

    Updated middleware

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8080;
 const api_routes = require('./routes/api_routes');
 const html_routes = require('./routes/html_routes');
 
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger('dev'));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static('public'));
 
+// Connect to the Mongo DB
+mongoose.connect('mongodb://localhost/unit18Populater', { useNewUrlParser: true });
+
+// Routes
 api_routes(app);
 html_routes(app);
 
 // Set Handlebars.
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // listen on port 8080
 app.listen(PORT, () => console.log('Listening on http://localhost:' + PORT));
