d99f839fac723b1bc6cc85f799e62e13fe612de0
Donna Kuang <dolaku@gmail.com>
2019-07-05 16:22:00 -0400

    Working on getting notes in

--- public/js/news.js
+++ public/js/news.js
@@ -1,19 +1,59 @@
-// Click 'Scrape Now!' button
-$('#scrape-now').on('click', () => {
-    $.ajax({
-        method: 'GET',
-        url: '/scrape'
+$(document).ready(function () {
+    // Click 'Scrape Now!' button
+    $('#scrape-now').on('click', () => {
+        $.ajax({
+            method: 'GET',
+            url: '/scrape'
+        });
+
     });
 
-});
+
+    $(document).on('click', '.user-notes', function () {
+        let articleID = $(this).attr('data-id');
+        console.log(articleID);
+        
+        $.ajax({
+            method: "GET",
+            url: "/articles/" + articleID
+        }).then(function (data) {
+            if (data.note) {
+                $('.note-title').val(data.note.title);
+                $('.note-body').val(data.note.body);
+            }
+        })
+    });
+
+    $(document).on('click', '.save-note', function () {
+        let articleID = $(this).attr('data-id');
+        console.log(articleID);
+
+        $.ajax({
+            method: "POST",
+            url: "/articles/" + articleID,
+            data: {
+                // Value taken from title input
+                title: $('.note-title').val(),
+                // Value taken from note textarea
+                body: $('.note-body').val()
+            }
+        })
+            // With that done
+            .then(function (response) {
+                // Log the response
+                console.log(response);
+                // Empty the notes section
+                $("#notes").empty();
+            });
+    });
 
 
-// Grab the articles as a json
-$.getJSON("/api/articles", (data) => {
-    // For each one
-    for (let i = 0; i < data.length; i++) {
+    // Grab the articles as a json
+    $.getJSON("/api/articles", (data) => {
+        // For each one
+        for (let i = 0; i < data.length; i++) {
 
-        let articleCards = `
+            let articleCards = `
             <div class="card m-3">
                 <div class="card-body">
                     <div>
@@ -28,7 +68,8 @@ $.getJSON("/api/articles", (data) => {
 
                         <div class="btn btn-outline-success user-notes" 
                             data-toggle="collapse" 
-                            data-target="#data-${data[i]._id}" 
+                            data-target="#data-${data[i]._id}"
+                            data-id="${data[i]._id}"
                             aria-expanded="false" 
                             aria-controls="collapseExample">
                             Notes
@@ -37,27 +78,29 @@ $.getJSON("/api/articles", (data) => {
                         <div class="collapse mt-3" id="data-${data[i]._id}">
                             <form>
                                 <div class="form-group">
-                                    <input type="text" class="form-control mb-2" id="note-title-${data[i]._id}" placeholder="Note Title">
-                                    <textarea class="form-control" id="note-body-${data[i]._id}" rows="4" placeholder="Note description"></textarea>
+                                    <input type="text" class="form-control mb-2" id="note-title-${data[i]._id}" title="Title" placeholder="Note Title">
+                                    <textarea class="form-control" id="note-body-${data[i]._id}" rows="3" title="Note" placeholder="Note description"></textarea>
                                 </div>
+                                <div class="save-note btn btn-outline-success" data-id="${data[i]._id}">Save Note</div>
                             </form>
-                            <div class="save-note btn btn-outline-success" data-id="${data[i]._id}">Save Note</div>
                         </div>
                     </div>
                 </div>
             </div>
         `;
 
-        // Display the apropos information on the page
-        $("#articles").append(articleCards);
-    }
-});
+            // Display the apropos information on the page
+            $("#articles").append(articleCards);
+        }
+    });
 
-$.getJSON("/api/saved", (data) => {
-    // For each one
-    for (let i = 0; i < data.length; i++) {
 
-        let articleCards = `
+    // Grab the saved articles as a json
+    $.getJSON("/api/saved", (data) => {
+        // For each one
+        for (let i = 0; i < data.length; i++) {
+
+            let articleCards = `
             <div class="card m-3">
                 <div class="card-body">
                     <div>
@@ -72,7 +115,8 @@ $.getJSON("/api/saved", (data) => {
                         
                         <div class="btn btn-outline-success user-notes" 
                             data-toggle="collapse" 
-                            data-target="#data-${data[i]._id}" 
+                            data-target="#data-${data[i]._id}"
+                            data-id="${data[i]._id}"
                             aria-expanded="false" 
                             aria-controls="collapseExample">
                             Notes
@@ -82,17 +126,19 @@ $.getJSON("/api/saved", (data) => {
                             <form>
                                 <div class="form-group">
                                     <input type="text" class="form-control mb-2" id="note-title-${data[i]._id}" placeholder="Note Title">
-                                    <textarea class="form-control" id="note-body-${data[i]._id}" rows="4" placeholder="Note description"></textarea>
+                                    <textarea class="form-control" id="note-body-${data[i]._id}" rows="3" placeholder="Note description"></textarea>
                                 </div>
+                                <div class="save-note btn btn-outline-success" data-id="${data[i]._id}">Save Note</div>
                             </form>
-                            <div class="save-note btn btn-outline-success" data-id="${data[i]._id}">Save Note</div>
                         </div>
                     </div>
                 </div>
             </div>
         `;
 
-        // Display the apropos information on the page
-        $("#saved-articles").append(articleCards);
-    }
+            // Display the apropos information on the page
+            $("#saved-articles").append(articleCards);
+        }
+    });
+
 });
