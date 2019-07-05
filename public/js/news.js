0266c4ffc511b4c6f92aa310fe20d823d6a26f98
Donna Kuang <dolaku@gmail.com>
2019-07-05 11:55:12 -0400

    Added note form to each article

--- public/js/news.js
+++ public/js/news.js
@@ -12,7 +12,7 @@ $('#scrape-now').on('click', () => {
 $.getJSON("/api/articles", (data) => {
     // For each one
     for (let i = 0; i < data.length; i++) {
-       
+
         let articleCards = `
             <div class="card m-3">
                 <div class="card-body">
@@ -25,7 +25,24 @@ $.getJSON("/api/articles", (data) => {
                     </div>
                     <div class="text-right mt-4">
                         <div class="save-article btn btn-outline-info mr-2" data-id="${data[i]._id}">Save Article</div>
-                        <div class="add-note btn btn-outline-success" data-id="${data[i]._id}">Add Note</div>
+
+                        <div class="btn btn-outline-success user-notes" 
+                            data-toggle="collapse" 
+                            data-target="#data-${data[i]._id}" 
+                            aria-expanded="false" 
+                            aria-controls="collapseExample">
+                            Notes
+                        </div>
+
+                        <div class="collapse mt-3" id="data-${data[i]._id}">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" class="form-control mb-2" id="note-title-${data[i]._id}" placeholder="Note Title">
+                                    <textarea class="form-control" id="note-body-${data[i]._id}" rows="4" placeholder="Note description"></textarea>
+                                </div>
+                            </form>
+                            <div class="save-note btn btn-outline-success" data-id="${data[i]._id}">Save Note</div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -39,7 +56,7 @@ $.getJSON("/api/articles", (data) => {
 $.getJSON("/api/saved", (data) => {
     // For each one
     for (let i = 0; i < data.length; i++) {
-       
+
         let articleCards = `
             <div class="card m-3">
                 <div class="card-body">
@@ -52,7 +69,24 @@ $.getJSON("/api/saved", (data) => {
                     </div>
                     <div class="text-right mt-4">
                         <div class="remove-article btn btn-outline-info mr-2" data-id="${data[i]._id}">Remove Article</div>
-                        <div class="add-note btn btn-outline-success" data-id="${data[i]._id}">Add Note</div>
+                        
+                        <div class="btn btn-outline-success user-notes" 
+                            data-toggle="collapse" 
+                            data-target="#data-${data[i]._id}" 
+                            aria-expanded="false" 
+                            aria-controls="collapseExample">
+                            Notes
+                        </div>
+
+                        <div class="collapse mt-3" id="data-${data[i]._id}">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" class="form-control mb-2" id="note-title-${data[i]._id}" placeholder="Note Title">
+                                    <textarea class="form-control" id="note-body-${data[i]._id}" rows="4" placeholder="Note description"></textarea>
+                                </div>
+                            </form>
+                            <div class="save-note btn btn-outline-success" data-id="${data[i]._id}">Save Note</div>
+                        </div>
                     </div>
                 </div>
             </div>
